64447e186e5190ea037c6a345253c75f398df1c4
Rodrigo Weber <rodrigoweber97@hotmail.com>
2022-10-29 10:53:43 -0300

    gerando a lista de cursos dinamicamente

--- src/app/courses/course-list/course-list.component.ts
+++ src/app/courses/course-list/course-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CourseService } from '../course.service';
 import { Course } from './Course';
 
 @Component({
@@ -9,41 +10,9 @@ import { Course } from './Course';
 export class CourseListComponent implements OnInit {
   courses: Course[] = [];
 
-  constructor() {}
+  constructor(private courseService: CourseService) {}
 
   ngOnInit(): void {
-    this.courses = [
-      {
-        id: 1,
-        name: 'Angular Course',
-        imageUrl: 'assets/images/angular.svg',
-        price: 99.99,
-        code: 'XPS 8796',
-        duration: 120,
-        rating: 4.8,
-        releaseDate: '22/10/2022'
-      },
-      {
-        id: 2,
-        name: 'Java Course',
-        imageUrl: 'assets/images/java.png',
-        price: 149.99,
-        code: 'XPS 1324',
-        duration: 190,
-        rating: 4.7,
-        releaseDate: '22/10/2022'
-
-      },
-      {
-        id: 3,
-        name: 'MySQL Course',
-        imageUrl: 'assets/images/mysql.png',
-        price: 49.99,
-        code: 'XPS 5623',
-        duration: 50,
-        rating: 4.6,
-        releaseDate: '22/10/2022'
-      },
-    ];
+    this.courses = this.courseService.retrieveAll();
   }
 }
