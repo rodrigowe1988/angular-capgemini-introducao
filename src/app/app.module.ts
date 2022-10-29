98a6144a48ba5a5fffa49e7fb7ca5c0dd8e9a1a3
Rodrigo Weber <rodrigoweber97@hotmail.com>
2022-10-29 11:14:28 -0300

    criando NAVBAR e iniciando com ROTAS e PAGINA 404

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,35 @@ import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list/course-list.component';
 import { ReplacePipe } from './pipe/replace.pipe';
 import { StarComponent } from './star/star/star.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule } from '@angular/router';
+import { ErrorNotFoundComponent } from './error-not-found/error-not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, CourseListComponent, StarComponent, ReplacePipe],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [
+    AppComponent,
+    CourseListComponent,
+    StarComponent,
+    ReplacePipe,
+    NavbarComponent,
+    ErrorNotFoundComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        path: 'courses', component: CourseListComponent
+      },
+      {
+        path: '**', component: ErrorNotFoundComponent
+      }
+    ]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
