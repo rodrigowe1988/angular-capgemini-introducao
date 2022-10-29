4b2ed8f60f27f1e3c3cd9fa18b160e5a9820239c
Rodrigo Weber <rodrigoweber97@hotmail.com>
2022-10-29 10:19:46 -0300

    adicionando PIPES e PIPES CUSTOMIZADOS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list/course-list.component';
+import { ReplacePipe } from './pipe/replace.pipe';
 import { StarComponent } from './star/star/star.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CourseListComponent,
-    StarComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
+  declarations: [AppComponent, CourseListComponent, StarComponent, ReplacePipe],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
